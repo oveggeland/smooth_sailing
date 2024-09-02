4c5a642f2a648f07283c95963e09ec5bf2d75c00
Oskar <oveggeland@gmail.com>
2024-09-02 19:25:46 +0200

    Add cookbook script

---
+++ scripts/cookbook.py
@@ -0,0 +1,17 @@
+#! /bin/python3 
+
+import rosbag
+import sys
+
+if __name__ == "__main__":
+    in_bag = sys.argv[1]
+    out_bag = sys.argv[2]
+
+    with rosbag.Bag(out_bag, 'w') as outbag:
+        for topic, msg, t in rosbag.Bag(in_bag).read_messages():
+            # This also replaces tf timestamps under the assumption 
+            # that all transforms in the message share the same timestamp
+            if topic == "/tf" and msg.transforms:
+                outbag.write(topic, msg, msg.transforms[0].header.stamp)
+            else:
+                outbag.write(topic, msg, msg.header.stamp if msg._has_header else t)
