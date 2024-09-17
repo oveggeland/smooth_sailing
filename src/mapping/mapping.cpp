d4fc352accd4cb5bdf1e1e25f8119dd17bdfe3aa
Oskar <oveggeland@gmail.com>
2024-09-17 17:05:28 +0200

    Mapping clean up. Config yaml file. Split maps into sections of maximum intervals.

--- src/mapping/mapping.cpp
+++ src/mapping/mapping.cpp
@@ -1,29 +1,30 @@
-#include <gtsam/geometry/Pose3.h>
-#include <gtsam/geometry/Point3.h>
-#include <gtsam/geometry/Rot3.h>
 #include <map>
 #include <fstream>
-#include <iomanip>
+#include <filesystem>
 
 #include <ros/ros.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <sensor_msgs/PointCloud2.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl_conversions/pcl_conversions.h>
 
 #include <open3d/Open3D.h>
-#include <open3d/geometry/PointCloud.h>
-
 #include <open3d/core/Tensor.h>
 #include <open3d/t/geometry/PointCloud.h>
 
-#include <filesystem>
-namespace fs = std::filesystem;
+#include <gtsam/geometry/Pose3.h>
+#include <gtsam/geometry/Point3.h>
+#include <gtsam/geometry/Rot3.h>
 
+#include <yaml-cpp/yaml.h>
+
+namespace fs = std::filesystem;
 using namespace gtsam;
 using namespace std;
+
 
 Pose3 interpolatePose3(const gtsam::Pose3& pose1, const gtsam::Pose3& pose2, double alpha) {
     // Interpolate translation
@@ -33,8 +34,6 @@ Pose3 interpolatePose3(const gtsam::Pose3& pose1, const gtsam::Pose3& pose2, dou
     // Construct and return the interpolated Pose3
     return Pose3(interpolated_rotation, interpolated_position);
 }
-
-
 
 // Function to query pose for a given time
 bool queryPose(const std::map<double, gtsam::Pose3>& poses, double query_time, Pose3& pose_out) {
@@ -62,8 +61,6 @@ bool queryPose(const std::map<double, gtsam::Pose3>& poses, double query_time, P
     return true;
 }
 
-#include <yaml-cpp/yaml.h>
-
 // Read lidar to body transformation
 Pose3 readTbl(const std::string& filename){
     YAML::Node config = YAML::LoadFile(filename);
@@ -90,8 +87,8 @@ Pose3 readTbl(const std::string& filename){
 }
 
 
-
-std::map<double, Pose3> buildLidarPoseMap(const std::string& filename, Pose3 Tbl){
+// Build pose map (timestamp->pose) from navigation file and extrinsic matrix
+std::map<double, Pose3> buildPoseMap(const std::string& filename, Pose3 T){
     std::map<double, Pose3> poseMap;
     std::ifstream file(filename);
 
@@ -137,7 +134,7 @@ std::map<double, Pose3> buildLidarPoseMap(const std::string& filename, Pose3 Tbl
             Rot3 rotation = Rot3::RzRyRx(roll, pitch, yaw);
             Pose3(rotation, translation);
 
-            poseMap[ts] = Pose3(rotation, translation).compose(Tbl);
+            poseMap[ts] = Pose3(rotation, translation).compose(T);
         }
     }
 
@@ -146,115 +143,184 @@ std::map<double, Pose3> buildLidarPoseMap(const std::string& filename, Pose3 Tbl
 }
 
 
+class mapBuilder{
+public:
+    mapBuilder(){}
+    mapBuilder(const fs::path& ws, const Pose3& T, const YAML::Node& config): ws_(ws), ext_(T), config_(config){
+        pose_map_ = buildPoseMap(ws_ / "navigation/nav.csv", ext_);
 
-#define POINT_INTERVAL 10.0e-6 // microseconds
-#define MIN_X_DISTANCE 10
+        cloud_interval_ = config_["cloud_max_interval"].as<double>();
+        use_relative_stamps_ = config_["cloud_relative_timestamps"].as<bool>();
+        sample_rate_ = config["lidar_sample_rate"].as<double>();
+        frame_rate_ = config["lidar_frame_rate"].as<double>();
+        return_count_ = config["lidar_return_count"].as<int>();
+        min_x_distance_ = config["min_x_distance"].as<double>();
+
+        frame_interval_ = (1 / frame_rate_);
+        sample_interval_ = (1 / sample_rate_);
+
+        cloud_path_ = ws_ / "raw_clouds"; 
+        fs::create_directory(cloud_path_);
+    }
 
 
-void buildPointCloud(std::map<double, Pose3> lidarPoseMap, const std::string& bag_filename, std::string out_file, double max_interval) {
-    // Open the bag
-    rosbag::Bag bag(bag_filename);
-    double t_start = 0;
+    void addFrame(sensor_msgs::PointCloud2::ConstPtr cloud_msg){
+        // Get timestamps
+        double t0_frame = cloud_msg->header.stamp.toSec();
+        double t1_frame = t0_frame + frame_interval_;
 
-    // Set up a ROSBag view for the topic
-    std::string topic = "/livox_lidar_node/pointcloud2";
-    rosbag::View view(bag, rosbag::TopicQuery(topic));
-
-    // Set up vectors to save point info (We could probably do this a bit smarter but it does not seem like the allocation is out biggest problem)
-    std::vector<_Float64> positions;
-    std::vector<uint8_t> intensities;
-    std::vector<_Float64> timestamps;
-    std::vector<_Float32> distances;
-    
-    // Iterate over the messages
-    for (rosbag::MessageInstance const m : view) {
-        // Check if the message is a PointCloud2 message
-        sensor_msgs::PointCloud2::ConstPtr cloud_msg = m.instantiate<sensor_msgs::PointCloud2>();
-        if (cloud_msg != nullptr) {
-            pcl::PointCloud<pcl::PointXYZI> pcl_cloud;
-            pcl::fromROSMsg(*cloud_msg, pcl_cloud);
-
-            _Float64 t0 = cloud_msg->header.stamp.toSec();
-            _Float64 frame_interval = 2*pcl_cloud.size()*POINT_INTERVAL;
-            _Float64 t1 = t0 + frame_interval; // LiDAR in dual return mode
-
-            if (t_start == 0){
-                t_start = t0;
-            }
-            else if (t0 - t_start > max_interval){
-                break;
-            }
-            
-            // Initial and end pose of lidar frame, if not available, skip to next frame
-            Pose3 T0, T1;
-            if (!queryPose(lidarPoseMap, t0, T0) || !queryPose(lidarPoseMap, t1, T1)){
-                continue;
-            }
-
-            int cnt = 0;
-            for (const auto& point : pcl_cloud.points) {
-                if (point.x < MIN_X_DISTANCE){ // Rough outlier rejection
-                    cnt ++;
-                    continue; // Less than 10 meters away, skip point
-                }  
-
-                _Float32 distance2 = point.x*point.x + point.y*point.y + point.z*point.z;
-                _Float64 ts_point = t0 + (cnt/2)*POINT_INTERVAL;
-
-                Pose3 T = interpolatePose3(T0, T1, (ts_point - t0) / frame_interval);
-                gtsam::Point3 transformed_point = T.transformFrom(gtsam::Point3(point.x, point.y, point.z));
-
-                positions.push_back(transformed_point.x());
-                positions.push_back(transformed_point.y());
-                positions.push_back(transformed_point.z());
-
-                intensities.push_back(point.intensity);
-                distances.push_back(distance2); // Distance squared, to save computational costs
-                timestamps.push_back(ts_point);
-
-                cnt ++;
-            }
+        // Query poses
+        Pose3 T0, T1;
+        if (!queryPose(pose_map_, t0_frame, T0) || !queryPose(pose_map_, t1_frame, T1)){
+            return;
         }
 
-        if (!ros::ok())
-            exit(1);
+        // Extract cloud
+        pcl::PointCloud<pcl::PointXYZI> cloud;
+        pcl::fromROSMsg(*cloud_msg, cloud);
+        
+        // Iterate through and add points
+        int cnt = -1;
+        for (const auto& point : cloud.points) {
+            cnt ++;
+            if (point.x < min_x_distance_){ // Rough outlier rejection
+                continue; // Less than 10 meters away, skip point
+            }  
+
+            // Get point stamp and pose
+            double ts_point = t0_frame + (cnt/2)*sample_interval_;
+            Pose3 T = interpolatePose3(T0, T1, (ts_point - t0_frame) / frame_interval_);
+
+            // Transform to world frame
+            gtsam::Point3 transformed_point = T.transformFrom(gtsam::Point3(point.x, point.y, point.z));
+            positions_.push_back(transformed_point.x());
+            positions_.push_back(transformed_point.y());
+            positions_.push_back(transformed_point.z());
+
+            // Save auxillary attributes (Intensity, distance2, and timestamps)
+            intensities_.push_back(point.intensity);
+            distances_.push_back(point.x*point.x + point.y*point.y + point.z*point.z); // Distance squared, to save computational costs
+            if (use_relative_stamps_)
+                timestamps_.push_back(ts_point - t0_);
+            else
+                timestamps_.push_back(ts_point);
+        }
     }
-    bag.close();
 
-    std::unordered_map<std::string, open3d::core::Tensor> tensor_map;
 
-    tensor_map["positions"] = open3d::core::Tensor(positions, {(int)positions.size()/3, 3}, open3d::core::Dtype::Float64);
-    tensor_map["intensities"] = open3d::core::Tensor(intensities, {(int)intensities.size(), 1}, open3d::core::Dtype::UInt8);
-    tensor_map["timestamps"] = open3d::core::Tensor(timestamps, {(int)timestamps.size(), 1}, open3d::core::Dtype::Float64);
-    tensor_map["distances"] = open3d::core::Tensor(distances, {(int)distances.size(), 1}, open3d::core::Dtype::Float32);
-    open3d::t::geometry::PointCloud point_cloud(tensor_map);
+    void cloudTimeout(){
+        if (!positions_.empty()){
+            std::unordered_map<std::string, open3d::core::Tensor> tensor_map;
 
-    // Save the PointCloud to a file
-    if (!open3d::t::io::WritePointCloud(out_file, point_cloud))
-        cout << "Failed to save cloud at " << out_file << endl;
-}
+            tensor_map["positions"] = open3d::core::Tensor(positions_, {(int)positions_.size()/3, 3}, open3d::core::Dtype::Float64);
+            tensor_map["intensities"] = open3d::core::Tensor(intensities_, {(int)intensities_.size(), 1}, open3d::core::Dtype::UInt8);
+            tensor_map["timestamps"] = open3d::core::Tensor(timestamps_, {(int)timestamps_.size(), 1}, open3d::core::Dtype::Float64);
+            tensor_map["distances"] = open3d::core::Tensor(distances_, {(int)distances_.size(), 1}, open3d::core::Dtype::Float32);
+            open3d::t::geometry::PointCloud point_cloud(tensor_map);
 
+            // Save the PointCloud to file
+            string out_file = cloud_path_ / (std::to_string(cloud_cnt_) + "raw.ply");
+            cout << "Saving cloud to " << out_file << endl;;
+            if (!open3d::t::io::WritePointCloud(out_file, point_cloud))
+                cout << "Failed to save cloud at " << out_file << endl;
+
+
+            // Empty vectors (But keep capacity fixed)
+            positions_.clear();
+            intensities_.clear();
+            timestamps_.clear();
+            distances_.clear();
+        }
+        cloud_cnt_ ++;
+    }
+
+    void buildMaps(){
+        rosbag::Bag bag(ws_ / "cooked.bag");
+        rosbag::View view(bag, rosbag::TopicQuery(config_["lidar_topic"].as<string>()));
+
+        t0_ = view.getBeginTime().toSec();
+
+        for (rosbag::MessageInstance const m : view) {
+            sensor_msgs::PointCloud2::ConstPtr cloud_msg = m.instantiate<sensor_msgs::PointCloud2>();
+            if (cloud_msg != nullptr) {
+                double t0_frame = cloud_msg->header.stamp.toSec();
+
+                if (t0_map_ == 0.0){
+                    t0_map_ = t0_frame;
+                }
+                else if (t0_frame - t0_map_ > cloud_interval_){
+                    cout << fixed << setprecision(2);
+                    cout << t0_frame << "," << t0_map_ << ", " << cloud_interval_ << "," << t0_frame - t0_map_ << endl;
+                    cloudTimeout(); // Save cloud and reset
+                    t0_map_ = 0.0;
+                }
+                
+                addFrame(cloud_msg);
+            }
+
+            if (!ros::ok())
+                exit(1);
+        }
+        bag.close();
+    }
+
+
+private:
+    const fs::path ws_;
+    fs::path cloud_path_;
+
+    const Pose3 ext_; // Extrinsic matrix
+    const YAML::Node config_;
+
+    double sample_rate_;
+    double frame_rate_;
+    double sample_interval_;
+    double frame_interval_;
+
+    double min_x_distance_;
+    int return_count_;
+
+
+    std::map<double, Pose3> pose_map_;
+
+    double cloud_interval_;
+    int cloud_cnt_ = 0;
+    bool use_relative_stamps_;
+    double t0_ = 0.0;
+
+    double t0_map_ = 0.0;
+
+    vector<_Float64> positions_;
+    vector<uint8_t> intensities_;
+    vector<_Float64> timestamps_;
+    vector<_Float32> distances_;
+};
+
+
+// Entry point to node
 int main(int argc, char** argv){
     ros::init(argc, argv, "mapping_node");
     ros::NodeHandle nh("~");
     
+    // Find workspace
     std::string workspace;
     if (!nh.getParam("/ws", workspace)){
         cout << "Error: No workspace provided" << endl;
         exit(1);
     }
-    
+
+    // Find extrinsics
     std::string ext_file;
     nh.getParam("/ext_file", ext_file);
-    Pose3 Tbl = readTbl(ext_file);
 
-    std::string nav_file = fs::path(workspace) / "navigation" / "nav.csv"; 
-    std::map<double, Pose3> lidarPoseMap = buildLidarPoseMap(nav_file, Tbl);
-    
-    //double t_offset = YAML::LoadFile(fs::path(workspace) / "navigation" / "nav_info.yaml")["t0"].as<double>();
-    double max_interval;
-    nh.getParam("/max_time_interval", max_interval);
-    buildPointCloud(lidarPoseMap, fs::path(workspace) / "cooked.bag", fs::path(workspace) / "raw.ply", max_interval);
+    // Find extrinsics
+    std::string config;
+    nh.getParam("/map_config", config);
 
+    // Build map object
+    mapBuilder mb(workspace, readTbl(ext_file), YAML::LoadFile(config));
+    mb.buildMaps();
+
+    //buildPointCloud(lidarPoseMap, fs::path(workspace) / "cooked.bag", fs::path(workspace) / "raw.ply", max_interval);
     return 0;
 }
